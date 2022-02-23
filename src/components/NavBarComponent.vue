6b8e0e2902d6481227a5d1e544aafed1df89f73e
Velmoren <velmoren@yahoo.com>
2022-02-23 22:52:12 +0300

    cicle

---
+++ src/components/NavBarComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <ul
+      class="
+        header
+        d-flex
+        justify-content-center justify-content-md-start
+        flex-wrap
+      "
+    >
+
+      <nav-item
+        :link="links.header.link"
+        classLink="header__item"
+      >
+        <img
+          :src="require(`@/assets/logo/${links.header.icon}`)"
+          :alt="links.header.icon"
+        />
+      </nav-item>
+
+      <nav-item
+        v-for="link in links.other"
+        :key="link.id"
+        :link="link.link"
+        :text="link.text"
+        classLink="header__item"
+      ></nav-item>
+      
+    </ul>
+  </header>
+</template>
+
+<script>
+import NavItem from "@/components/NavItem";
+
+export default {
+  components: { NavItem },
+  computed: {
+    links() {
+      return this.$store.getters["getHeaderLinks"]
+    }
+  }
+};
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import HeroView from "../views/HeroView"
+import OurCoffeeView from "../views/OurCoffeeView"
+import ForYourPleasureView from "../views/ForYourPleasureView"
+import ContactUsView from "../views/ContactUsView"
+import ThankYouView from "../views/ThankYouView"
+
+Vue.use(VueRouter)
+
+const routes = [
+    { path: '/', component: HeroView },
+    { path: '/our-coffee', component: OurCoffeeView },
+    { path: '/for-your-pleasure', component: ForYourPleasureView },
+    { path: '/contact-us', component: ContactUsView },
+    { path: '/thank-you', component: ThankYouView },
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes 
+})
+
+export default router
